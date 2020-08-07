6845891342873821cbf095a37434f64198f67be0
James <jamescscalise@gmail.com>
2020-08-07 00:25:51 -0400

    arranged file

---
+++ precache-manifest.cdde34b8b536fc9dd026e01d5350ad05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ceea71e4e967ab3f406a65feaa242",
+    "url": "/crossword-frontend/index.html"
+  },
+  {
+    "revision": "38028885502d1526782f",
+    "url": "/crossword-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "640ec2cf6c1cc9cda2a0",
+    "url": "/crossword-frontend/static/js/2.5f8862c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crossword-frontend/static/js/2.5f8862c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38028885502d1526782f",
+    "url": "/crossword-frontend/static/js/main.7c28a5e5.chunk.js"
+  },
+  {
+    "revision": "4e0e62cb4f848d0dd33c",
+    "url": "/crossword-frontend/static/js/runtime-main.dcba6284.js"
+  }
+]);
